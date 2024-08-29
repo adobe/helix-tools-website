dd933703a95f8d1dd4566967aaa7fef051a12e6d
Amol Anand <amol.anand@gmail.com>
2024-08-28 20:28:05 -0400

    error handling in case site-url isn't good for whatever reason

--- tools/log-viewer/scripts.js
+++ tools/log-viewer/scripts.js
@@ -134,8 +134,8 @@ function updateTableDisplay(show, table = document.querySelector('table')) {
 async function writeLoginMessage(owner, repo) {
   if (owner && repo) {
     // check if project is available at .aem.page
-    let siteUrl = document.querySelector('form input#site-url')?.value;
-    if (siteUrl) siteUrl = new URL(siteUrl).hostname;
+    let siteUrl = document.querySelector('form input#site-url')?.value || `https://main--${repo}--${owner}.hlx.page/`;
+    siteUrl = new URL(siteUrl)?.hostname;
     return `You need to <a href="https://${siteUrl}/" target="_blank">sign in to the ${repo} project sidekick</a> to view the requested logs.`;
   }
   if (repo) {
