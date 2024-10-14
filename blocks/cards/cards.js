6df2e13f1261b1d9bdc0b4aff428dc787e46ab75
fkakatie <ksexton@adobe.com>
2024-10-14 10:38:32 -0600

    fix: link card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,25 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
+  // convert cards to list and list items
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
+    li.append(...row.children);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  // decorate card content
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(
+    createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]),
+  ));
+  ul.querySelectorAll(':scope > li a[href]:first-of-type').forEach((a) => {
+    const li = a.closest('li');
+    li.addEventListener('click', () => a.click());
+  });
   block.textContent = '';
   block.append(ul);
 }
