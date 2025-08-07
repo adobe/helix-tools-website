8c1442fa52e31ecee403de565988dfbfcf637c81
rofe <github@rofe.com>
2025-08-07 17:07:47 +0200

    feat: logout

--- utils/login.js
+++ utils/login.js
@@ -1,4 +1,8 @@
-import { messageSidekick, NO_SIDEKICK } from './sidekick.js';
+import {
+  messageSidekick,
+  getSidekickId,
+  NO_SIDEKICK,
+} from './sidekick.js';
 
 /**
  * Returns a login button element for the given organization and site.
@@ -6,12 +10,11 @@ import { messageSidekick, NO_SIDEKICK } from './sidekick.js';
  * @param {string} cfg.org The organization name
  * @param {string} cfg.site The site name
  * @param {Function} [cfg.callback] The callback function to call after login
- * @param {boolean} [cfg.quiet] Whether to use a quiet login button
- * @param {string} [cfg.text] The override text to display on the login button
+ * @param {number} [cfg.status] The HTTP status code
  * @returns {HTMLElement} The login button element
  */
 export default async function createLoginButton({
-  org, site, callback, quiet, text,
+  org, site, callback, status = 401,
 }) {
   const authInfo = await new Promise((resolve) => {
     messageSidekick({ action: 'getAuthInfo' }, (res) => resolve(res));
@@ -23,11 +26,14 @@ export default async function createLoginButton({
     msg.innerHTML = 'Install <a href="https://chromewebstore.google.com/detail/aem-sidekick/igkmdomcgoebiipaifhmpfjhbjccggml" target="_blank" rel="noopener noreferrer">AEM Sidekick</a> to sign in.';
     return msg;
   }
-  const loginText = text || (authInfo?.includes(org) && !quiet ? 'Switch account' : 'Sign in');
+
+  const loggedIn = authInfo?.includes(org) || status === 200;
+  const loginText = loggedIn ? 'Sign out' : 'Sign in';
 
   const loginButton = document.createElement('button');
+  loginButton.id = 'login';
   loginButton.classList.add('button', 'login');
-  if (quiet) {
+  if (status === 403) {
     loginButton.classList.add('quiet');
   }
   loginButton.title = loginText;
@@ -36,18 +42,24 @@ export default async function createLoginButton({
   // trigger login on button click (alt key for microsoft IDP with common tenant)
   loginButton.addEventListener('click', async () => {
     loginButton.disabled = true;
-    const altKey = loginButton.classList.contains('ops');
-    const success = await messageSidekick({
-      action: 'login',
-      org,
-      site,
-      idp: altKey ? 'microsoft' : undefined,
-      tenant: altKey ? 'common' : undefined,
-      selectAccount: authInfo?.includes(org),
-    });
-    if (typeof callback === 'function') {
-      callback(success);
+    const api = loggedIn ? 'logout' : 'login';
+    const loginUrl = new URL(`https://admin.hlx.page/${api}/${org}/${site}/main`);
+    if (!loggedIn) {
+      const altKey = loginButton.classList.contains('ops');
+      loginUrl.searchParams.append('idp', altKey ? 'microsoft' : '');
+      loginUrl.searchParams.append('tenant', altKey ? 'common' : '');
+      loginUrl.searchParams.append('selectAccount', authInfo?.includes(org) || altKey);
     }
+    loginUrl.searchParams.append('extensionId', getSidekickId());
+    const loginWindow = window.open(loginUrl.toString(), '_blank');
+    const checkInterval = setInterval(() => {
+      if (loginWindow.closed) {
+        clearInterval(checkInterval);
+        if (typeof callback === 'function') {
+          callback();
+        }
+      }
+    }, 1000);
   });
 
   // add body class if alt key is pressed
--- utils/sidekick.js
+++ utils/sidekick.js
@@ -3,6 +3,14 @@ const SIDEKICK_ID = 'igkmdomcgoebiipaifhmpfjhbjccggml';
 export const NO_SIDEKICK = 'no-sidekick';
 
 /**
+ * Gets the Sidekick ID from localStorage or uses the default.
+ * @returns {string} The Sidekick ID.
+ */
+export function getSidekickId() {
+  return localStorage.getItem('aem-sidekick-id')?.trim() || SIDEKICK_ID;
+}
+
+/**
  * Sends a message to the Sidekick extension.
  * @param {Object} message The message
  * @param {Function} [callback] The callback function
@@ -13,7 +21,7 @@ export async function messageSidekick(message, callback) {
     if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
       let messageResolved = false;
       chrome.runtime.sendMessage(
-        localStorage.getItem('aem-sidekick-id')?.trim() || SIDEKICK_ID,
+        getSidekickId(),
         message,
         (response) => {
           if (response) {
